739a04239cb7518cc60c775dbf4c5486fd49a537
Zachary Hanif <zachary.hanif@capitalone.com>
2019-11-07 17:29:26 -0500

    Added more endpoints

--- src/server.py
+++ src/server.py
@@ -1,10 +1,28 @@
 from flask import Flask
  
-app = Flask(__name__) 
- 
+app = Flask(__name__)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if flask.request.method == 'POST':
+        return 'Submitted experiment.'
+
+    return '''<form method="POST">
+                  Data: <input type="text" name="data"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+
+@app.route('/compare/<path:experiment_one>/<path:experiment_two>')
+def compare():
+    return 'This is the compare function for two experiments.'
+
+@app.route('/experiment')
+def experiment():
+    return 'This is the view of an experiment'
+
 @app.route('/hello')
 def hello():
-    return ‘Hello!’
+    return 'Hello!'
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
