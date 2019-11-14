dca294503040901836ca818e478f9e8cb43b33bd
rashmisankepally <rashmi.sankepally@gmail.com>
2019-11-14 11:55:21 -0500

    just checking

--- src/server.py
+++ src/server.py
@@ -9,6 +9,7 @@ POSTGRES_PW = "818"
 POSTGRES_URL = "127.0.0.1:5432"
 POSTGRES_DB = "project"
 
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if flask.request.method == 'POST':
@@ -19,18 +20,22 @@ def upload():
                   <input type="submit" value="Submit"><br>
               </form>'''
 
+
 @app.route('/compare/<path:experiment_one>/<path:experiment_two>')
 def compare():
     return 'This is the compare function for two experiments.'
+
 
 @app.route('/experiment')
 def experiment():
     return 'This is the view of an experiment'
 
+
 @app.route('/hello')
 def hello():
     return 'Hello!'
- 
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
     # Connect to an existing database
